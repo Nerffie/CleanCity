35cf6bc8acacd03d9c564fbf83843075f39a7f22
hamzaElhaddaoui <hamza.el.haddaoui@gmail.com>
2019-10-13 01:25:54 +0100

    Merge branch 'master' of F:\Projet\CleanCity with conflicts.

---
+++ app/src/main/java/com/example/cleancity/BellActivity.java
@@ -0,0 +1,34 @@
+package com.example.cleancity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.cleancity.RecyclerViewItems.MyAdapter;
+
+import java.util.ArrayList;
+
+public class BellActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter myAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bell);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerSignal);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        myAdapter = new MyAdapter();
+        recyclerView.setAdapter(myAdapter);
+
+//        myAdapter= new MyAdapter();
+//        recyclerView.setAdapter(myAdapter);
+        //définit l'agencement des cellules, ici de façon verticale, comme une ListView
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
